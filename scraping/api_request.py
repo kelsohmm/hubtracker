c126d1e346adda1a2fb459652199416deb2c4569
Bogusz <stefanczykb@gmail.com>
2018-05-21 22:31:55 +0200

    changed api request builder to fallback to yesterday date

--- scraping/api_request.py
+++ scraping/api_request.py
@@ -1,15 +1,29 @@
-import datetime
+from datetime import datetime, timedelta
 from config import API_CONFIG
 
 _ENDPOINT_TEMPLATE = "https://api.hubstaff.com/v1/custom/by_date/team?start_date={0}&end_date={0}"
 _USER_TOKEN_HEADER = "Auth-Token"
 _APP_TOKEN_HEADER = "App-Token"
 _ORGANIZATION_HEADER = "organizations"
+_DATE_FORMAT = '%Y-%m-%d'
 
 
-def build_activity_report_query(requested_date, config=API_CONFIG):
+def build_activity_report_query(requested_date=None, config=API_CONFIG, now_func=datetime.now):
+    if not _is_valid_date_format(requested_date):
+        requested_date=_get_yesterday_date(now_func)
+
     endpoint = _ENDPOINT_TEMPLATE.format(requested_date)
     return endpoint, _build_headers(config)
+
+def _get_yesterday_date(now_func):
+    return datetime.strftime(now_func() - timedelta(1), _DATE_FORMAT)
+
+def _is_valid_date_format(requested_date):
+    try:
+        datetime.strptime(requested_date, _DATE_FORMAT)
+        return True
+    except:
+        return False
 
 
 def _build_headers(config):
--- test/scraping/test_api_request.py
+++ test/scraping/test_api_request.py
@@ -1,4 +1,7 @@
 from unittest import TestCase
+
+from datetime import datetime
+
 from scraping.api_request import build_activity_report_query
 
 
@@ -13,9 +16,19 @@ TEST_CONFIG_HEADERS = {
     "organizations": [111],
 }
 
+def now_mock():
+    return datetime(2018, 1, 2)
+
+
 class ApiRequestBuildingTest(TestCase):
     def test_should_build_request_with_headers_for_valid_date_string(self):
-        endpoint, headers = build_activity_report_query('2018-01-01', config=TEST_CONFIG)
+        endpoint, headers = build_activity_report_query('2018-01-03', config=TEST_CONFIG, now_func=now_mock)
+
+        self.assertEqual(endpoint, "https://api.hubstaff.com/v1/custom/by_date/team?start_date=2018-01-03&end_date=2018-01-03")
+        self.assertEqual(headers, TEST_CONFIG_HEADERS)
+
+    def test_should_use_yesterday_date_when_date_empty(self):
+        endpoint, headers = build_activity_report_query(config=TEST_CONFIG, now_func=now_mock)
 
         self.assertEqual(endpoint, "https://api.hubstaff.com/v1/custom/by_date/team?start_date=2018-01-01&end_date=2018-01-01")
         self.assertEqual(headers, TEST_CONFIG_HEADERS)
