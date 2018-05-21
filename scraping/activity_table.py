1d3f1abc104cdf1e856b57e334d585795eb27bac
Bogusz <stefanczykb@gmail.com>
2018-05-21 20:17:49 +0200

    added test for 2 projects with full implementation

--- scraping/activity_table.py
+++ scraping/activity_table.py
@@ -2,12 +2,49 @@ from datetime import timedelta
 
 
 def build_activity_table(report_entries, empty_token=" "):
-    [only_entry] = report_entries
+    projects_indexes, users_indexes = _decide_table_header_indexes(report_entries)
+    table = _build_zero_filled(empty_token, projects_indexes, users_indexes)
+    _fill_cell_values(table, report_entries, projects_indexes, users_indexes)
+    return _stringify_durations(table)
 
-    return [
-        [empty_token, only_entry.user.name],
-        [only_entry.project.name, to_time_string(only_entry.duration)]
-    ]
 
-def to_time_string(duration):
+def _stringify_durations(table):
+    for row in range(1, len(table)):
+        for col in range(1, len(table[0])):
+            table[row][col] = _to_time_string(table[row][col])
+    return table
+
+def _fill_cell_values(table, report_entries, projects_indexes, users_indexes):
+    for entry in report_entries:
+        row = projects_indexes[entry.project]
+        col = users_indexes[entry.user]
+        table[row][col] = entry.duration
+
+def _build_zero_filled(empty_token, projects_indexes, users_indexes):
+    table = _two_dimensional_list_of_zeros(cols=len(users_indexes.keys()) + 1, rows=len(projects_indexes.keys()) + 1)
+    table[0][0] = empty_token
+
+    for user, index in users_indexes.items():
+        table[0][index] = user.name
+
+    for project, index in projects_indexes.items():
+        table[index][0] = project.name
+
+    return table
+
+def _two_dimensional_list_of_zeros(cols, rows):
+    return [[0] * cols for _ in range(rows)]
+
+def _decide_table_header_indexes(report_entries):
+    projects_set = {entry.project for entry in report_entries}
+    users_set  = {entry.user for entry in report_entries}
+    projects_sorted = sorted(projects_set, key=lambda project: project.id)
+    users_sorted = sorted(users_set, key=lambda user: user.id)
+
+    return (
+        {project: index for index, project in enumerate(projects_sorted, start=1)},
+        {user: index for index, user in enumerate(users_sorted, start=1)}
+    )
+
+def _to_time_string(duration):
     return str(timedelta(seconds=duration))
--- test/scraping/test_activity_table.py
+++ test/scraping/test_activity_table.py
@@ -8,18 +8,38 @@ from scraping.activity_table import build_activity_table
 EMPTY=" "
 ActivityTableTestData = namedtuple("ActivityTableTestData", ["input", "output"])
 
-SINGLE_ENTRY = ActivityTableTestData(
-    input=[
-ReportEntry(PROJECT1, USER1, TIME_10SEC_INT)
-    ],output=[
-[EMPTY         , USER1_NAME] ,
-[PROJECT1_NAME , TIME_10SEC_STR]
-    ])
-
 class ReportsParsingTest(TestCase):
     def _run_test_for(self, test_data):
         self.assertEqual(test_data.output, build_activity_table(test_data.input, empty_token=EMPTY))
 
     def test_should_build_2_dim_table_for_single_entry(self):
-        self._run_test_for(SINGLE_ENTRY)
+        self._run_test_for(ActivityTableTestData(
+            input=[
+                ReportEntry(PROJECT1, USER1, TIME_10SEC_INT)
+            ],output=[
+                [EMPTY         , USER1_NAME] ,
+                [PROJECT1_NAME , TIME_10SEC_STR]
+            ]))
+
+    def test_should_build_2_rows_for_2_projects(self):
+        self._run_test_for(ActivityTableTestData(
+            input=[
+                ReportEntry(PROJECT1, USER1, TIME_10SEC_INT),
+                ReportEntry(PROJECT2, USER1, TIME_10SEC_INT),
+            ],output=[
+                [EMPTY         , USER1_NAME] ,
+                [PROJECT1_NAME , TIME_10SEC_STR],
+                [PROJECT2_NAME , TIME_10SEC_STR],
+            ]))
+
+    def test_should_sort_by_project_id(self):
+        self._run_test_for(ActivityTableTestData(
+            input=[
+                ReportEntry(PROJECT2, USER1, TIME_10SEC_INT),
+                ReportEntry(PROJECT1, USER1, TIME_10SEC_INT),
+            ],output=[
+                [EMPTY         , USER1_NAME] ,
+                [PROJECT1_NAME , TIME_10SEC_STR],
+                [PROJECT2_NAME , TIME_10SEC_STR],
+            ]))
 
