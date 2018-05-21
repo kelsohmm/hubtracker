4bdce9b9d7e9387714e047d629b131fb1ea5dbf2
Bogusz <stefanczykb@gmail.com>
2018-05-21 22:35:21 +0200

    renamed private funcs in activity_report

--- scraping/activity_report.py
+++ scraping/activity_report.py
@@ -3,21 +3,19 @@ import json
 from scraping.types import UserKey, ProjectKey, ReportEntry
 
 
-def get_projects(report):
-    return report['organizations'][0]['projects']
-
-
-def get_project_users(project):
-    return project['dates'][0]['users']
-
-
 def get_report_entries_from_json(report_json):
     result = []
-    for project in get_projects(json.loads(report_json)):
+    for project in _get_projects(json.loads(report_json)):
         project_key = ProjectKey(project['id'], project['name'])
 
-        for user in get_project_users(project):
+        for user in _get_project_users(project):
             user_key = UserKey(user['id'], user['name'])
             result.append(ReportEntry(project_key, user_key, user['duration']))
 
     return result
+
+def _get_projects(report):
+    return report['organizations'][0]['projects']
+
+def _get_project_users(project):
+    return project['dates'][0]['users']
