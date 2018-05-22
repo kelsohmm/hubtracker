f1f4ca050ea93677a73bf4aeb8c428b76885c46c
Bogusz <stefanczykb@gmail.com>
2018-05-22 22:52:57 +0200

    extracted date validation, moved to view

--- test/scraping/test_request_building.py
+++ test/scraping/test_request_building.py
@@ -1,7 +1,4 @@
 from unittest import TestCase
-
-from datetime import datetime
-
 from scraping.request_building import build_activity_report_query
 
 
@@ -14,25 +11,10 @@ TEST_CONFIG_HEADERS = {
     "App-Token": "XYZ",
 }
 
-def now_mock():
-    return datetime(2018, 1, 2)
-
 
 class ApiRequestBuildingTest(TestCase):
     def test_should_build_request_with_headers_for_valid_date_string(self):
-        endpoint, headers = build_activity_report_query('2018-01-03', config=TEST_CONFIG, now_func=now_mock)
+        endpoint, headers = build_activity_report_query('2018-01-03', config=TEST_CONFIG)
 
         self.assertEqual(endpoint, "https://api.hubstaff.com/v1/custom/by_project/team?start_date=2018-01-03&end_date=2018-01-03")
-        self.assertEqual(headers, TEST_CONFIG_HEADERS)
-
-    def test_should_use_yesterday_date_when_date_none(self):
-        endpoint, headers = build_activity_report_query(None, config=TEST_CONFIG, now_func=now_mock)
-
-        self.assertEqual(endpoint, "https://api.hubstaff.com/v1/custom/by_project/team?start_date=2018-01-01&end_date=2018-01-01")
-        self.assertEqual(headers, TEST_CONFIG_HEADERS)
-
-    def test_should_use_yesterday_date_when_date_incorrect(self):
-        endpoint, headers = build_activity_report_query('2018-02-30',config=TEST_CONFIG, now_func=now_mock)
-
-        self.assertEqual(endpoint, "https://api.hubstaff.com/v1/custom/by_project/team?start_date=2018-01-01&end_date=2018-01-01")
         self.assertEqual(headers, TEST_CONFIG_HEADERS)
--- webapp/views.py
+++ webapp/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 
 from scraping.api_request import request_activity_table
+from scraping.dateutils import make_valid_date
 
 
 def index_view(request):
     return date_view(request, None)
 
 def date_view(request, date):
+    date = make_valid_date(date)
+
     return render(
         request,
         'index.html',
